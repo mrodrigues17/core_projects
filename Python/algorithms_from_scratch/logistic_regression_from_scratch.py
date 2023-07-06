a9ad2fd7cc1188d5791a661184900bb1e4f7ffd5
Max Rodrigues <mrod1791@gmail.com>
2023-07-05 21:40:06 -0500

    Add data files and init commits for regression in python #2

---
+++ Python/algorithms_from_scratch/logistic_regression_from_scratch.py
@@ -0,0 +1,11 @@
+class LogisticRegression():
+    pass
+
+    # def initial_coefficients(self, )
+
+
+
+import pandas as pd
+
+train = pd.read_csv("Python/Algorithms from scratch/data/titanic_train.csv")
+print(train.head())
